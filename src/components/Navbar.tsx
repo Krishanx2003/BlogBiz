d5f0b8f227e409893ae890085a805c547212cea9
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-09-11 01:22:00 +0530

    Initial commit from Create Next App

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useSession, signOut } from 'next-auth/react';
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
+  const { data: session } = useSession();
+
+  return (
+    <nav className="bg-black/[0.96] p-4 antialiased bg-grid-white/[0.02]">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <Link href="/" className="text-neutral-300 hover:text-neutral-200">
+          MyApp
+        </Link>
+        <Link href="/posts" className="text-neutral-300 hover:text-neutral-200">
+                Blogs
+              </Link>
+        <div className="flex space-x-4">
+          {session ? (
+            <>
+              <Link href="/create" className="text-neutral-300 hover:text-neutral-200">
+                create
+              </Link>
+              <button onClick={() => signOut()} className="text-neutral-300 hover:text-neutral-200">
+                Sign Out
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/login" className="text-neutral-300 hover:text-neutral-200">
+                Login
+              </Link>
+              <Link href="/register" className="text-neutral-300 hover:text-neutral-200">
+                Register
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
